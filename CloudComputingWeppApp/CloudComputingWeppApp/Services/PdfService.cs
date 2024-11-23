7f879c9672be5153b6a0483e6c1c0cde61330d35
mmmichail <163916611+mmmichail@users.noreply.github.com>
2024-11-23 12:17:46 +0200

    Add files via upload

---
+++ CloudComputingWeppApp/CloudComputingWeppApp/Services/PdfService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CloudComputingWeppApp.Services
+{
+    public class PdfService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PdfService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> OnPostSubmitAsync(string inputText)
+        {
+            var client = _httpClientFactory.CreateClient("RedactionService");
+            var response = await client.PostAsync("api/redact", new StringContent(inputText));
+
+            if (response.IsSuccessStatusCode)
+            {
+                var pdfData = await response.Content.ReadAsByteArrayAsync();
+                return File(pdfData, "application/pdf", "redacted.pdf");
+            }
+
+            return Page();
+        }
+    }
+}
