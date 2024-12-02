34b311b915a8572e61b1771631bbba4f63cfd187
mmmichail <163916611+mmmichail@users.noreply.github.com>
2024-12-02 18:43:45 +0200

    Update Program.cs

--- CloudComputing/CloudComputing/Program.cs
+++ CloudComputing/CloudComputing/Program.cs
@@ -1,3 +1,5 @@
+using Consul;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -17,5 +19,32 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Add Consul client
+builder.Services.AddSingleton<IConsulClient, ConsulClient>(p => new ConsulClient(config =>
+{
+    config.Address = new Uri(builder.Configuration["Consul:ConsulAddress"]);
+}));
+
+// Register service with Consul
+app.Lifetime.ApplicationStarted.Register(() =>
+{
+    var consulClient = app.Services.GetRequiredService<IConsulClient>();
+    var registration = new AgentServiceRegistration()
+    {
+        ID = builder.Configuration["Consul:ServiceId"],
+        Name = builder.Configuration["Consul:ServiceName"],
+        Address = "localhost",
+        Port = 5000
+    };
+    consulClient.Agent.ServiceRegister(registration).Wait();
+});
+
+// Deregister on shutdown
+app.Lifetime.ApplicationStopped.Register(() =>
+{
+    var consulClient = app.Services.GetRequiredService<IConsulClient>();
+    consulClient.Agent.ServiceDeregister(builder.Configuration["Consul:ServiceId"]).Wait();
+});
 
 app.Run();
